a4285d80ceae88f2979e9d8618283afbeb31754e
sntanju <sntanju07@gmail.com>
2021-10-01 21:17:19 +0600

    react-router-and-hook

---
+++ src/components/NotFound/NotFound.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const NotFound = () => {
+    return (
+        <div>
+            <h2>This is NotFound Page</h2>
+        </div>
+    );
+};
+
+export default NotFound;
